02cce4a9b35631cb1eb44fe91e0d29cab0f4bf16
Tiffany Kong <tkong3@uci.edu>
2025-03-08 04:09:09 +0000

    edit distance function + gtests

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -1,8 +1,23 @@
 #include <gtest/gtest.h>
 
-#include "dijkstras.h"
-#include "ladder.h"
+#include "../src/dijkstras.h"
+#include "../src/ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+TEST(LadderTests, EditDistanceWithin) {
+    string word1 = "cat";
+    string word2 = "cut";
+    EXPECT_TRUE(edit_distance_within(word1, word2, 1));
+    word2 = "cast";
+    EXPECT_TRUE(edit_distance_within(word1, word2, 1));
+    word2 = "ct";
+    EXPECT_TRUE(edit_distance_within(word1, word2, 1));
+    word2 = "casts";
+    EXPECT_FALSE(edit_distance_within(word1, word2, 1));
+
+}
+
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,3 +4,28 @@
 void error(string word1, string word2, string msg) {
     cout << "Error: " << msg << " with " << word1 << " and " << word2 << "\n";
 }
+
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    int str1_len = str1.size();
+    int str2_len = str2.size();
+    if (abs(str1_len - str2_len) > d) return false;
+    vector<int> prev(str2_len + 1), curr(str2_len + 1);
+
+    for (int i = 0; i <= str2_len; ++i) {
+        prev[i] = i;
+    }
+    for (int j = 1; j <= str1_len; ++j) {
+        curr[0] = j;
+
+        int lower = max(1, j - d);
+        int upper = min(str2_len, j + d);
+
+        for (int n = lower; n <= upper; n++) {
+            if (str1[j - 1] == str2[n - 1]) curr[n] = prev[n - 1];
+            else curr[n] = 1 + min({prev[n], curr[n - 1], prev[n - 1]});
+        }
+        prev.swap(curr);
+    }
+    return prev[str2_len] >= d;
+}
